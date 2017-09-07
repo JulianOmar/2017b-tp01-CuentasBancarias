39bb32a64e4e994507acae3aa77f624d92f27444
Meza Julian <julianmezare5@gmail.com>
2017-09-06 22:02:53 -0300

    AZUL: Mejora del algoritmo e individualización de los test

--- src/Tests/TestOperaciones.java
+++ src/Tests/TestOperaciones.java
@@ -1,29 +1,46 @@
 package Tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import cuentasBancarias.CuentaBanc;
 
 public class TestOperaciones {
 
-	/*
-	 * CuentaBanc c;
-	 * 
-	 * @Before 
-	 * public void setUp() { 
-	 * c = new CuentaBanc(); 
-	 * }
-	 */
-
+	
+	  CuentaBanc a;
+	  CuentaBanc b;
+	  
+	  @Before 
+	  public void setUp() { 
+		  a=new CuentaBanc(123,525);
+		  b=new CuentaBanc(124,100);//cuenta de destino 
+	  }
+	 
 	@Test
 	public void testDePrueba() {
-		CuentaBanc a=new CuentaBanc(123,525);
-		CuentaBanc b=new CuentaBanc(124,100);//cuenta de destino
+		Assert.assertEquals(1, a.transferirMontoHacia(520,b));
 		
-		Assert.assertEquals(-1, a.transferirMontoHacia(526,b));
-		
+		System.out.println("A.Cuenta: "+a.mostrarCta()+"\tSaldo: "+a.mostrarSdo());
+		System.out.println("B.Cuenta: "+b.mostrarCta()+"\tSaldo: "+b.mostrarSdo());
 				
+	}
+	
+	@Test
+	public void testSaldoNegativo() {
+		
+		Assert.assertEquals(-1, a.transferirMontoHacia(-520,b));
+	}
+	
+	@Test
+	public void testAutoTrans() {
+		Assert.assertEquals(-1, a.transferirMontoHacia(520,a));
+	}
+	
+	@Test
+	public void testSaldoMenor() {
+		Assert.assertEquals(-1, a.transferirMontoHacia(526,b));
 	}
 
 }
--- src/cuentasBancarias/CuentaBanc.java
+++ src/cuentasBancarias/CuentaBanc.java
@@ -13,14 +13,14 @@ public class CuentaBanc {
 	/*cuentaOrigen.transferirMontoHacia(monto, cuentaDestino);*/
 	public int transferirMontoHacia(int monto,CuentaBanc cc)
 	{
-		if(monto <0 || this.saldo< monto)
-			return -1;
-		if(this.nCuenta != cc.nCuenta)
+		if(monto >=0 && this.saldo>monto && this.nCuenta != cc.nCuenta)
 		{
-		cc.saldo+=monto;
-		this.saldo-=monto;
+			cc.saldo+=monto;
+			this.saldo-=monto;
+			return 1;			
 		}
-		return 1;
+			return -1;
+
 	}
 	
 	
